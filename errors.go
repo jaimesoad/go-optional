55991984dd097f15305cd1376af14d3cf4665c27
jaimesoad <jaime.ac1106@gmail.com>
2023-09-24 23:17:50 -0500

    Breaking changes, changed to opt.

---
+++ errors.go
@@ -0,0 +1,21 @@
+package opt
+
+import "log"
+
+func Log(err error) {
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
+
+func Fatal(err error) {
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func Panic(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
